fb86bb58066290b3b2b76f4e167d1719addaa035
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2024-10-08 13:28:57 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { isPlatformBrowser, NgIf } from '@angular/common';
+import { WebcamModule, WebcamImage } from 'ngx-webcam';
+import { Subject } from 'rxjs';
+import { FaceAuthService } from './face-auth.service';  // Certifique-se de ajustar o caminho
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [NgIf, WebcamModule, HttpClientModule],
+  providers: [FaceAuthService],
+  template: `
+<div>
+  <h3>Leitor de Digital</h3>
+  <button (click)="authenticate()">Ler Impressão Digital</button>
+</div>
+
+  `
 })
 export class AppComponent {
-  title = 'source';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  async authenticate() {
+    if (isPlatformBrowser(this.platformId)) {
+      try {
+        // Gerar um desafio aleatório
+        const challenge = new Uint8Array(32);
+        window.crypto.getRandomValues(challenge);
+
+        // Define as opções para a autenticação
+        const publicKey: any = {
+          challenge: challenge,
+          allowCredentials: [{
+            id: new Uint8Array(32), // ID da chave registrada
+            type: 'public-key'
+          }],
+          timeout: 60000,
+          userVerification: "preferred",
+        };
+
+        // Inicia a autenticação
+        const credential = await navigator.credentials.get({ publicKey });
+        console.log('Credenciais obtidas:', credential);
+
+        // Aqui você pode processar as credenciais conforme necessário
+      } catch (error) {
+        console.error('Erro ao autenticar:', error);
+      }
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter, RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withFetch()),
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(routes),
+  ]
+});
