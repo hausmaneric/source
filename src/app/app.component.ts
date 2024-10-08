f05ff753adcc284d41740fdfe95d25d386de201b
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2024-10-08 13:32:29 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,12 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs';
   standalone: true,
   imports: [NgIf, WebcamModule, HttpClientModule],
   providers: [FaceAuthService],
-  template: `
-<div>
-  <h3>Leitor de Digital</h3>
-  <button (click)="authenticate()">Ler Impressão Digital</button>
-</div>
-
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  statusMessage: string = ''; // Mensagem de status para o usuário
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   async authenticate() {
@@ -33,21 +30,29 @@ export class AppComponent {
         const publicKey: any = {
           challenge: challenge,
           allowCredentials: [{
-            id: new Uint8Array(32), // ID da chave registrada
+            id: new Uint8Array(32), // ID da chave registrada, substitua conforme necessário
             type: 'public-key'
           }],
           timeout: 60000,
-          userVerification: "preferred",
+          userVerification: "preferred", // Verificação do usuário preferencial
         };
+
+        this.statusMessage = 'Aguardando autenticação...';
 
         // Inicia a autenticação
         const credential = await navigator.credentials.get({ publicKey });
-        console.log('Credenciais obtidas:', credential);
+
+        // Exibir credenciais obtidas
+        this.statusMessage = 'Autenticação bem-sucedida!';
 
         // Aqui você pode processar as credenciais conforme necessário
+        console.log('Credenciais obtidas:', credential);
       } catch (error) {
+        this.statusMessage = 'Erro ao autenticar: ' + error;
         console.error('Erro ao autenticar:', error);
       }
+    } else {
+      this.statusMessage = 'Este recurso não está disponível neste dispositivo.';
     }
   }
 }
